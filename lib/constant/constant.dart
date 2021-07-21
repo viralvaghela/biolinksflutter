6a5d620b6829ae49e0acaf2b1c3f0a73ac51839f
viralvaghela <viralvaghela7109@gmail.com>
2021-07-21 18:20:18 +0530

    other links add

--- lib/constant/constant.dart
+++ lib/constant/constant.dart
@@ -3,4 +3,10 @@ class Constant {
       "https://instagram.famd5-2.fna.fbcdn.net/v/t51.2885-19/s150x150/193234773_926572198176448_4959766466891696352_n.jpg?_nc_ht=instagram.famd5-2.fna.fbcdn.net&_nc_ohc=nrUr6IsVyO4AX9mZP_O&edm=ABfd0MgBAAAA&ccb=7-4&oh=0e8ef95b3036760b8a57e05f2320d338&oe=60FF913D&_nc_sid=7bff83";
 
   static String githubHandle = "https://github.com/viralvaghela";
+  static String twitterHandle = "https://twitter.com/mrcodingboy";
+  static String discord = "https://bit.ly/codingboydiscord";
+  static String blog = "https://codingboy.in";
+  static String linkedinHandle = "https://www.linkedin.com/in/viralv";
+  static String facebookHandle = "https://www.facebook.com/thecodingboy";
+  static String portfolio = "https://viralvaghela.github.io";
 }
--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -10,73 +10,77 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[850],
-      body: Column(
-        // mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(height: 10),
-          Text(
-            "Coding Boy 💻",
-            style: const TextStyle(
-              fontSize: 45.0,
-              fontFamily: 'Agne',
-              color: Colors.blue,
-            ),
-          ),
-          SizedBox(height: 5),
-          Center(
-            child: SizedBox(
-              child: DefaultTextStyle(
-                style: const TextStyle(
-                  fontSize: 20.0,
-                  fontFamily: 'Agne',
-                  color: Colors.white,
-                ),
-                child: AnimatedTextKit(
-                  animatedTexts: [
-                    TypewriterAnimatedText(
-                        'Talk is cheap,show me the code - Linus Torvalds'),
-                  ],
-                  onTap: () {
-                    print("Tap Event");
-                  },
-                ),
-              ),
-            ),
-          ),
-          SizedBox(height: 5),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 20.0),
-            child: Container(
-              height: 100,
-              width: 100,
-              child: CircleAvatar(
-                backgroundImage: AssetImage("assets/profile.jpg"),
-              ),
-            ),
-          ),
-          Center(
-            child: InkWell(
-              hoverColor: Colors.blue,
-              splashColor: Colors.grey[850],
-              focusColor: Colors.blue,
-              onTap: () async {
-                await launch(Constant.githubHandle);
-              },
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 25),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
               child: Container(
-                width: 500,
                 height: 100,
-                child: Card(
-                  child: SizedBox(
-                    child: DefaultTextStyle(
-                      style: const TextStyle(fontSize: 20.0),
-                      child: Center(
-                        child: ListTile(
-                          leading: FaIcon(
-                            FontAwesomeIcons.github,
-                            size: 45,
+                width: 100,
+                child: CircleAvatar(
+                  backgroundImage: AssetImage("assets/profile.jpg"),
+                ),
+              ),
+            ),
+            Text(
+              "Coding Boy 💻",
+              style: const TextStyle(
+                  fontSize: 45.0, fontFamily: 'Agne', color: Colors.blue),
+            ),
+            SizedBox(height: 10),
+            Center(
+              child: SizedBox(
+                child: DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 20.0,
+                    fontFamily: 'Agne',
+                    color: Colors.white,
+                  ),
+                  child: AnimatedTextKit(
+                    repeatForever: true,
+                    animatedTexts: [
+                      TypewriterAnimatedText('Content Creator'),
+                      TypewriterAnimatedText('Bug Hunter'),
+                      TypewriterAnimatedText('Flutter Enthusiast'),
+                      TypewriterAnimatedText('Bug Hunter'),
+                      TypewriterAnimatedText('CSE Student'),
+                      TypewriterAnimatedText('Tech Geek'),
+                    ],
+                    onTap: () {
+                      print("Tap Event");
+                    },
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 15),
+            Center(
+              child: InkWell(
+                hoverColor: Colors.blue,
+                splashColor: Colors.grey[850],
+                focusColor: Colors.blue,
+                onTap: () async {
+                  await launch(Constant.githubHandle);
+                },
+                child: Container(
+                  width: 500,
+                  height: 100,
+                  child: Card(
+                    child: SizedBox(
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 20.0),
+                        child: Center(
+                          child: ListTile(
+                            leading: FaIcon(
+                              FontAwesomeIcons.github,
+                              size: 45,
+                            ),
+                            title: Text("My Github"),
                           ),
-                          title: Text("My Github"),
                         ),
                       ),
                     ),
@@ -84,8 +88,143 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-          )
-        ],
+
+            //Twitter
+            SizedBox(height: 15),
+            Center(
+              child: InkWell(
+                hoverColor: Colors.blue,
+                splashColor: Colors.grey[850],
+                focusColor: Colors.blue,
+                onTap: () async {
+                  await launch(Constant.twitterHandle);
+                },
+                child: Container(
+                  width: 500,
+                  height: 100,
+                  child: Card(
+                    child: SizedBox(
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 20.0),
+                        child: Center(
+                          child: ListTile(
+                            leading: FaIcon(
+                              FontAwesomeIcons.twitter,
+                              size: 45,
+                            ),
+                            title: Text("My Twitter"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            //Discord
+
+            SizedBox(height: 15),
+            Center(
+              child: InkWell(
+                hoverColor: Colors.blue,
+                splashColor: Colors.grey[850],
+                focusColor: Colors.blue,
+                onTap: () async {
+                  await launch(Constant.discord);
+                },
+                child: Container(
+                  width: 500,
+                  height: 100,
+                  child: Card(
+                    child: SizedBox(
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 20.0),
+                        child: Center(
+                          child: ListTile(
+                            leading: FaIcon(
+                              FontAwesomeIcons.discord,
+                              size: 45,
+                            ),
+                            title: Text("Join Discord Server"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            //Blog
+
+            SizedBox(height: 15),
+            Center(
+              child: InkWell(
+                hoverColor: Colors.blue,
+                splashColor: Colors.grey[850],
+                focusColor: Colors.blue,
+                onTap: () async {
+                  await launch(Constant.blog);
+                },
+                child: Container(
+                  width: 500,
+                  height: 100,
+                  child: Card(
+                    child: SizedBox(
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 20.0),
+                        child: Center(
+                          child: ListTile(
+                            leading: FaIcon(
+                              FontAwesomeIcons.blog,
+                              size: 45,
+                            ),
+                            title: Text("Blog"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+            //Porfolio
+
+            SizedBox(height: 15),
+            Center(
+              child: InkWell(
+                hoverColor: Colors.blue,
+                splashColor: Colors.grey[850],
+                focusColor: Colors.blue,
+                onTap: () async {
+                  await launch(Constant.portfolio);
+                },
+                child: Container(
+                  width: 500,
+                  height: 100,
+                  child: Card(
+                    child: SizedBox(
+                      child: DefaultTextStyle(
+                        style: const TextStyle(fontSize: 20.0),
+                        child: Center(
+                          child: ListTile(
+                            leading: FaIcon(
+                              FontAwesomeIcons.code,
+                              size: 45,
+                            ),
+                            title: Text("My Portfolio"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
